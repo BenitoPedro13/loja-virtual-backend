ec7e5cf8e4036369f150386a6da9b25b4c8d0388
Benito Pedro <benitopedro21@outlook.com>
2023-06-21 15:11:22 -0300

    Controller Permissao criado

--- src/main/java/com/dev/backend/controller/PermissaoController.java
+++ src/main/java/com/dev/backend/controller/PermissaoController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/permissao")
-public class PermissaoController { 
+public class PermissaoController {
 
     @Autowired
     private PermissaoService permissaoService;
