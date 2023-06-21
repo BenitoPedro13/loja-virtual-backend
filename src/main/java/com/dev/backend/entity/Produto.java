ccc3420636ae3e3083ff451830a91a60cbd741b6
Benito Pedro <benitopedro21@outlook.com>
2023-06-21 13:35:36 -0300

    Entity Produto criada

---
+++ src/main/java/com/dev/backend/entity/Produto.java
@@ -0,0 +1,34 @@
+package com.dev.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "produto")
+@Data
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String nome;
+    private String descricaoCurta;
+    private String descricaoLonga;
+    private Double valorCusto;
+    private Double valorVenda;
+    private String cep;
+
+    @ManyToOne
+    @JoinColumn(name = "idMarca")
+    private Marca marca;
+    @ManyToOne
+    @JoinColumn(name = "idCategoria")
+    private Categoria categoria;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataCriacao;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dataAtualizacao;
+}
