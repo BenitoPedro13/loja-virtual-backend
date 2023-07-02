227ad9fb56d9107dd5d255c94ae420faaf37c2b2
Benito Pedro <benitopedro21@outlook.com>
2023-07-02 12:26:14 -0300

    PessoaClienteService Adicionado

--- src/main/java/com/dev/backend/service/PessoaClienteService.java
+++ src/main/java/com/dev/backend/service/PessoaClienteService.java
@@ -2,14 +2,11 @@ package com.dev.backend.service;
 
 import com.dev.backend.dto.PessoaClienteRequestDTO;
 import com.dev.backend.entity.Pessoa;
-import com.dev.backend.repository.PermissaoRepository;
 import com.dev.backend.repository.PessoaClienteRepository;
-import com.dev.backend.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
-import java.util.List;
 
 @Service
 public class PessoaClienteService {
@@ -20,13 +17,18 @@ public class PessoaClienteService {
     @Autowired
     private PermissaoPessoaService permissaoPessoaService;
 
+    @Autowired
+    private EmailService emailService;
+
     public Pessoa inserir(PessoaClienteRequestDTO pessoaClienteRequestDTO){
         Pessoa pessoa = new PessoaClienteRequestDTO().converter(pessoaClienteRequestDTO);
 
         pessoa.setDataCriacao(new Date());
         Pessoa pessoaNova = pessoaRepository.saveAndFlush(pessoa);
         permissaoPessoaService.vincularPessoaPermissaoCliente((pessoaNova));
+        emailService.enviarEmailTexto(pessoaNova.getEmail(), "Cadastro na Loja Concluido.", "O registro na loja foi realizado com sucesso!. Em breve voce recebera a senha de acesso por E-Mail.");
         return pessoaNova;
     }
+
 
 }
