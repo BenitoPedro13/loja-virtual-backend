eefa0c7f00beed00738dd8d2f05e184c5c78e8b0
Benito Pedro <benitopedro21@outlook.com>
2023-07-02 14:39:39 -0300

    Template Emails added

--- src/main/java/com/dev/backend/service/EmailService.java
+++ src/main/java/com/dev/backend/service/EmailService.java
@@ -1,10 +1,18 @@
 package com.dev.backend.service;
 
+
+import freemarker.template.Configuration;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+
+import java.util.Map;
 
 @Service
 public class EmailService {
@@ -12,11 +20,13 @@ public class EmailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    @Autowired
+    private Configuration fmConfiguration;
+
     @Value("${spring.mail.username}")
     private String remetente;
 
     public String enviarEmailTexto(String destinatario, String titulo, String mensagem) {
-
         try {
             SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
             simpleMailMessage.setFrom(remetente);
@@ -24,9 +34,41 @@ public class EmailService {
             simpleMailMessage.setSubject(titulo);
             simpleMailMessage.setText(mensagem);
             javaMailSender.send(simpleMailMessage);
-            return "Email enviado!";
-        } catch (Exception exception) {
-            return "Erro ao enviar o Email!";
+            return "Email enviado";
+        } catch (Exception ex) {
+            return "Erro ao enviar o email";
+        }
+
+    }
+
+    public void enviarEmailTemplate(String destinatario, String titulo, Map<String, Object> propriedades) {
+        MimeMessage mimeMessage =javaMailSender.createMimeMessage();
+        try {
+
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+
+            mimeMessageHelper.setSubject(titulo);
+            mimeMessageHelper.setFrom(remetente);
+            mimeMessageHelper.setTo(destinatario);
+
+            mimeMessageHelper.setText(getConteudoTemplate(propriedades), true);
+
+            javaMailSender.send(mimeMessageHelper.getMimeMessage());
+        } catch (MessagingException e) {
+            e.printStackTrace();
         }
     }
+
+    public String getConteudoTemplate(Map < String, Object >model)     {
+        StringBuffer content = new StringBuffer();
+
+        try {
+            content.append(FreeMarkerTemplateUtils.processTemplateIntoString(fmConfiguration.getTemplate("email-recuperacao-codigo.flth"), model));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return content.toString();
+    }
+
+
 }
