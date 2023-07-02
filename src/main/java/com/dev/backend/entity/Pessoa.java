91bc3042350eaa8682dbfb7d3bb8cab5db0ebcc4
Benito Pedro <benitopedro21@outlook.com>
2023-07-02 11:12:51 -0300

    Added: Pessoa

--- src/main/java/com/dev/backend/entity/Pessoa.java
+++ src/main/java/com/dev/backend/entity/Pessoa.java
@@ -1,9 +1,13 @@
 package com.dev.backend.entity;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Setter;
 
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "pessoa")
@@ -24,6 +28,18 @@ public class Pessoa {
     @ManyToOne
     @JoinColumn(name = "idCidade")
     private Cidade cidade;
+
+    @OneToMany(mappedBy = "pessoa", orphanRemoval = true, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @Setter(value = AccessLevel.NONE)
+    @JsonManagedReference
+    private List<PermissaoPessoa> permissaoPessoas;
+    public void setPermissaoPessoas(List<PermissaoPessoa> pp) {
+        for (PermissaoPessoa p:pp) {
+            p.setPessoa(this);
+        }
+        this.permissaoPessoas = pp;
+    }
+
     @Temporal(TemporalType.TIMESTAMP)
     private Date dataCriacao;
     @Temporal(TemporalType.TIMESTAMP)
