b50d462c389e870135e8d2b755f961176cee0849
Benito Pedro <benitopedro21@outlook.com>
2023-07-02 13:15:32 -0300

    PessoaGerenciamentoService added

---
+++ src/main/java/com/dev/backend/service/PessoaGerenciamentoService.java
@@ -0,0 +1,37 @@
+package com.dev.backend.service;
+
+import com.dev.backend.entity.Pessoa;
+import com.dev.backend.repository.PessoaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Service
+public class PessoaGerenciamentoService {
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+
+    @Autowired
+    private EmailService emailService;
+
+    public String solicitarCodigo(String email) {
+        Pessoa pessoa = pessoaRepository.findByEmail(email);
+        pessoa.setCodigoRecuperacaoSenha(getCodigoRecuperacaoSenha(pessoa.getId()));
+        pessoa.setDataEnvioCodigo(new Date());
+        pessoaRepository.saveAndFlush(pessoa);
+        emailService.enviarEmailTexto(pessoa.getEmail(), "Codigo de recuperacao de senha", "Ola, o seu codigo para recuperaçao de senha e o seguinte: " + pessoa.getCodigoRecuperacaoSenha());
+        return "Codigo Enviado!";
+    }
+
+    private String getCodigoRecuperacaoSenha(Long id) {
+        DateFormat format = new SimpleDateFormat("ddMMyyyyHHmmssmm");
+        return format.format(new Date()) + id;
+    }
+
+
+}
