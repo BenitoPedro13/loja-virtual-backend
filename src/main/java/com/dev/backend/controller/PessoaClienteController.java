1aec2f4bb1ef240dccd4cd41117908cf3c332258
Benito Pedro <benitopedro21@outlook.com>
2023-07-02 11:11:44 -0300

    Added:
    PessoaClienteController

---
+++ src/main/java/com/dev/backend/controller/PessoaClienteController.java
@@ -0,0 +1,20 @@
+package com.dev.backend.controller;
+
+import com.dev.backend.dto.PessoaClienteRequestDTO;
+import com.dev.backend.entity.Pessoa;
+import com.dev.backend.service.PessoaClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/cliente")
+public class PessoaClienteController {
+
+    @Autowired
+    private PessoaClienteService pessoaService;
+
+    @PostMapping("/")
+    public Pessoa inserir(@RequestBody PessoaClienteRequestDTO pessoaClienteRequestDTO) {
+        return pessoaService.inserir(pessoaClienteRequestDTO);
+    }
+}
