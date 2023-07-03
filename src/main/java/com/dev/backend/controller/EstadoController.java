feeb7759979f9c5bd8ad7b006e52e207340755b3
Benito Pedro <benitopedro21@outlook.com>
2023-07-03 20:24:26 -0300

    CORS Config and Route for Bulk Delete

--- src/main/java/com/dev/backend/controller/EstadoController.java
+++ src/main/java/com/dev/backend/controller/EstadoController.java
@@ -2,7 +2,6 @@ package com.dev.backend.controller;
 
 import com.dev.backend.entity.Estado;
 import com.dev.backend.service.EstadoService;
-import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,23 +16,35 @@ public class EstadoController {
     private EstadoService estadoService;
 
     @GetMapping("/")
+    @CrossOrigin("http://localhost:3000")
     public List<Estado> buscarTodos() {
         return estadoService.buscarTodos();
     }
 
     @PostMapping("/")
+    @CrossOrigin("http://localhost:3000")
     public Estado inserir(@RequestBody Estado estado) {
         return estadoService.inserir(estado);
     }
 
     @PutMapping("/")
+    @CrossOrigin("http://localhost:3000")
     public Estado alterar(@RequestBody Estado estado) {
         return  estadoService.alterar(estado);
     }
 
     @DeleteMapping("/{id}")
+    @CrossOrigin("http://localhost:3000")
     public ResponseEntity<Void> excluir(@PathVariable("id") Long id) {
         estadoService.excluir(id);
         return ResponseEntity.ok().build();
     }
+
+    @DeleteMapping("/batch")
+    @CrossOrigin("http://localhost:3000")
+    public ResponseEntity<Void> excluirVarios(@RequestBody List<Long> ids) {
+        estadoService.excluirVarios(ids);
+        return ResponseEntity.ok().build();
+    }
+
 }
