77e16d0a12a4b73a8e83ef7ec11a275ec0415215
Benito Pedro <benitopedro21@outlook.com>
2023-07-03 20:24:34 -0300

    CORS Config and Route for Bulk Delete

--- src/main/java/com/dev/backend/controller/MarcaController.java
+++ src/main/java/com/dev/backend/controller/MarcaController.java
@@ -16,23 +16,34 @@ public class MarcaController {
     private MarcaService marcaService;
 
     @GetMapping("/")
+    @CrossOrigin("http://localhost:3000")
     public List<Marca> buscarTodos() {
         return marcaService.buscarTodos();
     }
 
     @PostMapping("/")
+    @CrossOrigin("http://localhost:3000")
     public Marca inserir(@RequestBody Marca marca) {
         return marcaService.inserir(marca);
     }
 
     @PutMapping("/")
+    @CrossOrigin("http://localhost:3000")
     public Marca alterar(@RequestBody Marca marca) {
         return  marcaService.alterar(marca);
     }
 
     @DeleteMapping("/{id}")
+    @CrossOrigin("http://localhost:3000")
     public ResponseEntity<Void> excluir(@PathVariable("id") Long id) {
         marcaService.excluir(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/batch")
+    @CrossOrigin("http://localhost:3000")
+    public ResponseEntity<Void> excluirVarios(@RequestBody List<Long> ids) {
+        marcaService.excluirVarios(ids);
         return ResponseEntity.ok().build();
     }
 }
