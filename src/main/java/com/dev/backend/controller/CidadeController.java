b54c7f97b53d4985de544908fcd7ef819a1092bd
Benito Pedro <benitopedro21@outlook.com>
2023-07-03 20:24:17 -0300

    CORS Config and Route for Bulk Delete

--- src/main/java/com/dev/backend/controller/CidadeController.java
+++ src/main/java/com/dev/backend/controller/CidadeController.java
@@ -2,7 +2,6 @@ package com.dev.backend.controller;
 
 import com.dev.backend.entity.Cidade;
 import com.dev.backend.service.CidadeService;
-import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,23 +16,34 @@ public class CidadeController {
     private CidadeService cidadeService;
 
     @GetMapping("/")
+    @CrossOrigin("http://localhost:3000")
     public List<Cidade> buscarTodos() {
         return cidadeService.buscarTodos();
     }
 
     @PostMapping("/")
+    @CrossOrigin("http://localhost:3000")
     public Cidade inserir(@RequestBody Cidade cidade) {
         return cidadeService.inserir(cidade);
     }
 
     @PutMapping("/")
+    @CrossOrigin("http://localhost:3000")
     public Cidade alterar(@RequestBody Cidade cidade) {
         return  cidadeService.alterar(cidade);
     }
 
     @DeleteMapping("/{id}")
+    @CrossOrigin("http://localhost:3000")
     public ResponseEntity<Void> excluir(@PathVariable("id") Long id) {
         cidadeService.excluir(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/batch")
+    @CrossOrigin("http://localhost:3000")
+    public ResponseEntity<Void> excluirVarios(@RequestBody List<Long> ids) {
+        cidadeService.excluirVarios(ids);
         return ResponseEntity.ok().build();
     }
 }
